6a709ade747c863d32c18bcb846e7a76009f1822
Yash Dhakad <yashvdkd@gmail.com>
2023-06-13 02:34:17 +0530

    feat & style: basic work done

--- src/components/CalcDisplay.jsx
+++ src/components/CalcDisplay.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const CalcDisplay = (props) => {
   return (
-    <div className='relative h-[120px] m-2 bg-sky-500 rounded-xl text-5xl text-white font-semibold shadow-inner shadow-sky-900'>
+    <div className='relative h-[120px] m-2 bg-sky-700 rounded-xl text-5xl text-sky-200 font-semibold shadow-inner shadow-sky-900'>
         <div className="m-2 absolute right-0 bottom-0 text-right">{props.num}</div>
     </div>
   )
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -3,7 +3,7 @@ import CalcFrame from './CalcFrame'
 
 const Main = () => {
   return (
-    <div className='max-w-screen-xl h-screen m-auto flex flex-row justify-center items-center bg-blue-400'>
+    <div className='max-w-screen-xl h-screen m-auto flex flex-row justify-center items-center bg-slate-900'>
         <CalcFrame />
     </div>
   )
